2ae102daff9bb55115730ce441ad00e42f1640bf
18dbrennan-rasmussen <18dbrennan-rasmussen@mlmc.vic.edu.au>
2017-11-23 12:04:07 +1100

    splicing introns

---
+++ splicing_introns.py
@@ -0,0 +1,6 @@
+my_dna = 'ATCGATCGATCGATCGACTGACTAGTCATAGCTATGCATGTAGCTACTCGATCGATCGATCGATCGATCGATCGATCGATCGATCATGCTATCATCGATCGATATCGATGCATCGACTACTAT'
+ex1 = my_dna [0:63]
+ex2 = my_dna [90:]
+print('Original seqence {0} \n'.format(my_dna))
+print('Coding ex 1: {0} \nCoding ex 2: {1}'.format(ex1, ex2))
+#used [] instead of {}
