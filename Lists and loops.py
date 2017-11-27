242d284e87e4a0fe31f6b7216ddebd5acc903c68
18dbrennan-rasmussen <18dbrennan-rasmussen@mlmc.vic.edu.au>
2017-11-27 14:20:47 +1100

    Lists and loops

--- Lists and loops.py
+++ Lists and loops.py
@@ -1,6 +1,14 @@
 words = []
 sentence = input('Enter a sentence')
 words = sentence.split(' ')
-print = (words)
+print(words)
 
+points = []
+gamepoints = input('Enter the points for each game played')
+points = gamepoints.split(',')
+print(points)
 
+total = 0
+for x in points:
+    total += int(x)
+    print(total)
