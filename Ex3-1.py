8f6bbe48f4c27c247af1ae7e91ca36f7411513e8
18dbrennan-rasmussen <18dbrennan-rasmussen@mlmc.vic.edu.au>
2017-11-27 16:32:46 +1100

    Ex3.1

---
+++ Ex3-1.py
@@ -0,0 +1,13 @@
+start
+open file for reading, infile = open(ex3-1_input.txt)
+open file for writing, outfile = open('trim.txt', 'w')
+
+for each line in file, for line in file
+    for each line in file find length
+    wanted <- length-13
+    output wanted to screen
+    output wanted to outfile
+
+close infile, infile.close
+close outfile, outfile.close
+stop
